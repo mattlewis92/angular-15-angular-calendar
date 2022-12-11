65029eca24d3b64e6ff472d684e26863e1c55a20
Matt Lewis <hello@mattlewis.me>
2022-12-11 17:07:49 +0000

    Try and reproduce issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-15-angular-calendar';
+  viewDate = new Date();
 }
